a1452af91fea777170e0de854af1c12294052ab1
Noahkiq <noahkiqgaming@gmail.com>
2019-05-02 22:08:45 -0400

    1.14

--- assets.py
+++ assets.py
@@ -1,8 +1,9 @@
 # Meta file used to obtain some of the vanilla assets (sounds/languages)
-# Get the textures from .minecraft/versions/1.13.2/1.13.2.jar
+# Get the textures from .minecraft/versions/1.14/1.14.jar
 import shutil
 import json
 import os
+import sys
 def makepath(path):
     if not os.path.exists(os.path.dirname(path)):
         try:
@@ -16,7 +17,7 @@ elif system.startswith('darwin'):
     destfolder = os.path.expanduser(os.path.join('~', 'Library', 'Application Support', 'minecraft'))
 elif system.startswith('win'):
     destfolder = os.path.expandvars(os.path.join('%APPDATA%', '.minecraft'))
-with open('/home/noahkiq/.minecraft/assets/indexes/1.13.1.json') as f:
+with open('/home/noahkiq/.minecraft/assets/indexes/1.14.json') as f:
     data = json.load(f)
 for key, value in data["objects"].items():
     if '/lang/' in key.lower() or '.ogg' in key.lower():
